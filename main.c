a54c5a0570315d117e5544a4a11cbe6fbbdd243e
Betsalel Williamson <bhw7@pitt.edu>
2017-11-01 21:43:14 -0400

    Updated main to with comments for section 3.

--- main.c
+++ main.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
 
+/* Section 2 of Project */
+/**
+ * Use Pthreads to create a working example of priority inversion and inheritance
+ */
+
+//
+//void function()
+//{
+//    //do some work here.
+//}
+//
+//void funtion1()
+//{
+//    //function 1 for thread 1.
+//    //do some work here
+//}
+//
+//void function2()
+//{
+////function 2 for thread 2.
+////do some work here and call function()
+//}
+//
+//void function3()
+//{
+////function 3 for thread 3.
+////do some work here
+//}
+//
+//void main()
+//{
+//    //create mutex and initialize it.
+////create thread 1, thread 2 and thread 3
+////set different priority for each thread
+////set handler for thread 1, thread 2 and thread 3 respectively. For instance, function 1 for thread 1, function 2 for thread 2 and function 3 for thread 3
+//}
+
 int main() {
     printf("Hello, World!\n");
     return 0;
